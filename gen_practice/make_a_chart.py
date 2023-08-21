17ca213896b9e6bc0d71fac8f93b8e154e45d57d
Molly Shove <molly.shove@microsoft.com>
2023-08-21 12:24:03 -0700

    Changed names to better fit naming conventions

--- gen_practice/make_a_chart.py
+++ gen_practice/make_a_chart.py
@@ -1,13 +1,13 @@
 
 # from key import key1
-import openai as oi
-key1 = ''
+import openai
+import os
 
 
 # Importing the libraries
 
 # Constants
-oi.api_key = key1
+openai.api_key = os.getenv("OPENAI_API_KEY")
 engine = "davinci"
 defualt_tokens = 10
 
@@ -19,7 +19,7 @@ def animal_fact_generator():
     # validate the input is a valid animal
     animal_validation_string = "is " + animal + \
         " a valid animal? respond with either 'yes' or 'no'"
-    check = oi.Completion.create(
+    check = openai.Completion.create(
         engine=engine, prompt=animal_validation_string, max_tokens=1)
 
     # if it's not a valid animal, return
@@ -30,7 +30,7 @@ def animal_fact_generator():
     # Prompt the user for a fact about the animal
     prompt = "Generate a random fun fact about" + \
         animal + "which is in the form of a statistic'?"
-    response = oi.Completion.create(
+    response = openai.Completion.create(
         engine=engine, prompt=prompt, max_tokens=defualt_tokens, temperature=0.2)
     print(response)
     # print(response['choices'][0]['text'])
