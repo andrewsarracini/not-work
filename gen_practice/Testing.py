b8966ebb03940b99c35e4dcca47d5cc9a0e5e782
Andrew Sarracini <andrewsarracini1@gmail.com>
2023-08-21 12:15:57 -0700

    fixed key

--- gen_practice/Testing.py
+++ gen_practice/Testing.py
@@ -1,14 +1,15 @@
 import openai
 import os
 
-animal = input('What animal would you like to learn about?')
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
 # openai.api_key = os.environ['OPENAI_API_KEY']
 
+
+animal = input('What animal would you like to learn about?')
+
 for chunk in openai.Completion.create(
     model="text-davinci-003",
-    prompt=f"make a joke about {animal}",
+    prompt=f"Lifespan of {animal}",
     max_tokens=30,
     temperature=0,
     stream=True
